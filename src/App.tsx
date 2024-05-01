9cc25dbcb532270d62bd26d6a908f1a23da2f50c
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-05-01 07:30:12 +0100

    Feat: Create tasks section when is empty

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Header } from "./components/Header";
 import { NewTaskInputForm } from "./components/NewTaskInpuForm";
 import { Tasks } from "./components/Tasks";
 import "./global.css";
+import style from "./App.module.css";
 
 export function App() {
 	return (
 		<main>
 			<Header />
 			<NewTaskInputForm />
+			<div className={style.tasks}>
+				<Tasks />
+			</div>
 		</main>
 	);
 }
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,8 +1,36 @@
-export function Tasks(){
+import { ClipboardText } from "phosphor-react";
+import style from "./Tasks.module.css";
 
-  return(
-    <section>
-      taskes
-    </section>
-  )
+import Clipboard from "../assets/Clipboard.svg";
+
+const task = [];
+
+export function Tasks() {
+	return (
+		<section className={style.container}>
+			<header className={style.header}>
+				<h3>
+					Tarefas Criadas <span>0</span>
+				</h3>
+				<h3>
+					Concluidas <span>0</span>
+				</h3>
+			</header>
+			<div className={style.tasks}>
+				{task.length === 0 ? (
+					<div className={style.empty}>
+						<img src={Clipboard} alt="" />
+						<div>
+							<p>
+								<span>Você ainda não tem tarefas cadastradas</span>
+								Crie tarefas e organize seus itens a fazer
+							</p>
+						</div>
+					</div>
+				) : (
+					<div>Task</div>
+				)}
+			</div>
+		</section>
+	);
 }
